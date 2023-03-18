7afcc6e91e1f89b47ae860f408d640bcb7902094
so_jaewon <soccat96@gmail.com>
2023-03-18 16:42:45 +0900

    first commit
    spring initializr - start.spring.io
    gradle 7.6.1
    java 11
    spring boot 2.7.9
    -lombok
    -spring web
    -spring data JPA
    -H2 Database

---
+++ src/main/java/kr/search/blog_search/BlogSearchApplication.java
@@ -0,0 +1,13 @@
+package kr.search.blog_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchApplication.class, args);
+    }
+
+}
