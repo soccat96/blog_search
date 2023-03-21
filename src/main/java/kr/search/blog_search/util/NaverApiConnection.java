90d9191c9c1ddee013e00265014a84129e46705b
so_jaewon <soccat96@gmail.com>
2023-03-22 02:53:20 +0900

    naver api - 블로그 검색 기능 추가
    naver api url
    naver api client id
    naver api client secret
    테스트 코드 작성

---
+++ src/main/java/kr/search/blog_search/util/NaverApiConnection.java
@@ -0,0 +1,33 @@
+package kr.search.blog_search.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Component
+public class NaverApiConnection {
+    @Value("${naver.client.id}")
+    private String NAVER_CLIENT_ID;
+
+    @Value("${naver.client.secret}")
+    private String NAVER_CLIENT_SECRET;
+
+    public HttpURLConnection searchBlogGetConnection(URL url) {
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("X-Naver-Client-Id", NAVER_CLIENT_ID);
+            connection.setRequestProperty("X-Naver-Client-Secret", NAVER_CLIENT_SECRET);
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            return connection;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/kr/search/blog_search/web/BlogControllerTest.java
+++ src/test/java/kr/search/blog_search/web/BlogControllerTest.java
@@ -1,6 +1,7 @@
 package kr.search.blog_search.web;
 
 import kr.search.blog_search.util.KakaoApiConnection;
+import kr.search.blog_search.util.NaverApiConnection;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -27,6 +28,12 @@ class BlogControllerTest {
     @Value("${kakao.dapi.host}")
     private String KAKAO_DAPI_HOST;
 
+    @Autowired
+    private NaverApiConnection naverApiConnection;
+
+    @Value("${naver.openapi.host}")
+    private String NAVER_OPENAPI_HOST;
+
     @Test
     public void kakao_블로그검색API_동작확인() throws IOException {
         URL urlCorrect = new URL(KAKAO_DAPI_HOST
@@ -50,5 +57,34 @@ class BlogControllerTest {
         assertThat(jsonObjectIncorrect.has("meta")).isTrue();
         assertThat(jsonObjectIncorrect.has("documents")).isTrue();
         assertThat((JSONArray) jsonObjectIncorrect.get("documents")).isEmpty();
+    }
+
+    @Test
+    public void 네이버_블로그검색API_동작확인() throws IOException {
+        URL urlCorrect = new URL(NAVER_OPENAPI_HOST
+                + "/v1/search/blog.json"
+                + "?query=" + URLEncoder.encode("가나다라마바사아자차카타파하", StandardCharsets.UTF_8)
+        );
+        URL urlIncorrect = new URL(NAVER_OPENAPI_HOST
+                + "/v1/search/blog.json"
+                + "?query=" + URLEncoder.encode("awiehfowfwubvuvbafpiuefvwuvfauofwfiew", StandardCharsets.UTF_8)
+        );
+
+        HttpURLConnection connectionCorrect = naverApiConnection.searchBlogGetConnection(urlCorrect);
+        assertThat(connectionCorrect.getResponseCode()).isEqualTo(200);
+        JSONObject jsonObjectCorrect = new JSONObject(new JSONTokener(connectionCorrect.getInputStream()));
+        assertThat(jsonObjectCorrect.has("total")).isTrue();
+        assertThat(jsonObjectCorrect.has("start")).isTrue();
+        assertThat(jsonObjectCorrect.has("display")).isTrue();
+        assertThat(jsonObjectCorrect.has("items")).isTrue();
+        assertThat((JSONArray) jsonObjectCorrect.get("items")).isNotEmpty();
+        HttpURLConnection connectionIncorrect = naverApiConnection.searchBlogGetConnection(urlIncorrect);
+        assertThat(connectionIncorrect.getResponseCode()).isEqualTo(200);
+        JSONObject jsonObjectIncorrect = new JSONObject(new JSONTokener(connectionIncorrect.getInputStream()));
+        assertThat(jsonObjectIncorrect.has("total")).isTrue();
+        assertThat(jsonObjectIncorrect.has("start")).isTrue();
+        assertThat(jsonObjectIncorrect.has("display")).isTrue();
+        assertThat(jsonObjectIncorrect.has("items")).isTrue();
+        assertThat((JSONArray) jsonObjectIncorrect.get("items")).isEmpty();
     }
 }
