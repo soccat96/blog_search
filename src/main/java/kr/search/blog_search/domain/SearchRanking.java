2e0e7d8f0ff84711ef130714e46fced2ecd6c75e
so_jaewon <soccat96@gmail.com>
2023-05-15 11:27:16 +0900

    SearchRanking 엔티티 수정
    constructor 매개변수 수정
    plusOneCount 메서드 명 변경
    bigint default 1을 정상 작동 시키려면 어떻게 해야 할까

--- src/main/java/kr/search/blog_search/domain/SearchRanking.java
+++ src/main/java/kr/search/blog_search/domain/SearchRanking.java
@@ -15,17 +15,16 @@ public class SearchRanking {
     private long id;
     @Column(unique = true)
     private String searchText;
-    @Column(columnDefinition = "bigint default 0", nullable = false)
+    @Column(columnDefinition = "bigint default 1", nullable = false)
     private long searchCount;
 
     @Builder
-    public SearchRanking(long id, String searchText, long searchCount) {
-        this.id = id;
+    public SearchRanking(String searchText) {
         this.searchText = searchText;
-        this.searchCount = searchCount;
+        this.searchCount = 1; // columnDefinition = "bigint default 1" is not working...
     }
 
-    public void plusCount() {
+    public void plusOneCount() {
         this.searchCount += 1;
     }
 }
