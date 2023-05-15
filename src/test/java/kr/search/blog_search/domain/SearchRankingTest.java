6844ee38ba002dc51828e3600a8e2ae5195a4865
so_jaewon <soccat96@gmail.com>
2023-05-15 11:28:47 +0900

    SearchRankingTest 수정
    엔티티의 public 에 관련한 테스트를 추가하고 남김

--- src/test/java/kr/search/blog_search/domain/SearchRankingTest.java
+++ src/test/java/kr/search/blog_search/domain/SearchRankingTest.java
@@ -25,6 +25,25 @@ class SearchRankingTest {
     }
 
     @Test
+    public void 생성자_확인() {
+        SearchRanking searchRanking = SearchRanking.builder()
+                .searchText("가나다라마바사")
+                .build();
+
+        assertThat(searchRanking.getSearchText()).isEqualTo("가나다라마바사");
+        assertThat(searchRanking.getSearchCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void 카운트_증가() {
+        SearchRanking sr = SearchRanking.builder().searchText("가나다라마바사").build();
+
+        sr.plusOneCount();
+
+        assertThat(sr.getSearchCount()).isEqualTo(2);
+    }
+
+    @Test
     public void 최초검색_결과_확인() {
         String searchText = "가나다라마바사아자차카타파하";
 
@@ -39,62 +58,5 @@ class SearchRankingTest {
         SearchRanking searchRanking = list.get(0);
         assertThat(searchRanking.getSearchText()).isEqualTo(searchText);
         assertThat(searchRanking.getSearchCount()).isEqualTo(1);
-    }
-
-    @Test
-    public void 검색_결과_카운트_증가() {
-        String searchText = "술마시고 노래하고 춤을 춰 봐도";
-
-        for(int i=0; i<5; i++) {
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText).build());
-        }
-        List<SearchRanking> list = searchRankingRepository.findAll();
-        assertThat(list.size()).isEqualTo(1);
-        SearchRanking searchRanking = list.get(0);
-        assertThat(searchRanking.getSearchText()).isEqualTo(searchText);
-        assertThat(searchRanking.getSearchCount()).isEqualTo(5);
-    }
-
-    @Test
-    public void 검색_결과_조회_by_카운트_desc_검색어_asc() {
-        String searchText01 = "가나다라";
-        String searchText02 = "마바사아";
-        String searchText03 = "자차카타";
-        String searchText04 = "파하";
-        String searchText05 = "가나다라마바사아";
-        String searchText06 = "자차카타파하";
-        String searchText07 = "07번째";
-        String searchText08 = "08번째";
-        String searchText09 = "09번째";
-        String searchText10 = "10번째";
-        String searchText11 = "11번째";
-
-        for(int i=0; i<10; i++) {
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText01).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText02).build());
-            if(i>8) continue;
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText03).build());
-            if(i>4) continue;
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText04).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText05).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText06).build());
-            if(i>0) continue;
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText07).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText08).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText09).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText10).build());
-            searchRankingService.saveOrUpdate(RequestDto.builder().query(searchText11).build());
-        }
-        List<SearchRanking> all = searchRankingRepository.findAll();
-        List<SearchRanking> top10 = searchRankingRepository.findTop10ByOrderBySearchCountDescSearchTextAsc();
-
-        assertThat(all.size()).isEqualTo(11);
-        assertThat(top10.size()).isEqualTo(10);
-        assertThat(top10.get(0).getSearchText()).isEqualTo(searchText01);
-        assertThat(top10.get(0).getSearchCount()).isEqualTo(10);
-        assertThat(top10.get(2).getSearchText()).isEqualTo(searchText03);
-        assertThat(top10.get(2).getSearchCount()).isEqualTo(9);
-        assertThat(top10.get(4).getSearchText()).isEqualTo(searchText06);
-        assertThat(top10.get(4).getSearchCount()).isEqualTo(5);
     }
 }
